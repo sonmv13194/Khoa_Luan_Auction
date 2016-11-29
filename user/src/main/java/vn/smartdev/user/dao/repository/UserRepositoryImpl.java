4e68d885a91795ee9a231623a37cb2989a3b4c99
HuyLeVu <huylv.itedu@gmail.com>
2016-11-29 15:02:45 +0700

    config pom

---
+++ user/src/main/java/vn/smartdev/user/dao/repository/UserRepositoryImpl.java
@@ -0,0 +1,7 @@
+//package vn.smartdev.user.dao.repository;
+//
+///**
+// * Created by levuhuy on 11/29/16.
+// */
+//public class UserRepositoryImpl implements UserRepository {
+//}
---
+++ user/src/main/test/java/vn/smartdev/user/test/UserTest.java
@@ -0,0 +1,7 @@
+package vn.smartdev.user.test;
+
+/**
+ * Created by levuhuy on 11/29/16.
+ */
+public class UserTest {
+}
