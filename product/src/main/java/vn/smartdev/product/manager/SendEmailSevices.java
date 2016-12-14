d87353d3ce8e2bf3c633edf1937fe755fe92afea
Toan <toannxm.itedu@gmail.com>
2016-12-14 10:45:07 +0700

    Toan

---
+++ product/src/main/java/vn/smartdev/product/manager/SendEmailSevices.java
@@ -0,0 +1,10 @@
+package vn.smartdev.product.manager;
+
+/**
+ * Created by Nhat on 12/12/2016.
+ */
+public interface SendEmailSevices {
+    public boolean setProperties(String userName,String password);
+    public boolean setMessage(String from,String to,String subject,String body);
+    public boolean sendEmail(String toEmail);
+}
