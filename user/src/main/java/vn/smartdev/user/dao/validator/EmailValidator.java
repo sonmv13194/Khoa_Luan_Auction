15b097bcea96609eaf066ee60f4cc960f99d8bde
HuyLeVu <levuhuy.it@gmail.com>
2017-01-03 10:17:53 +0700

    huy

---
+++ user/src/main/java/vn/smartdev/user/dao/validator/EmailValidator.java
@@ -0,0 +1,28 @@
+package vn.smartdev.user.dao.validator;
+
+import org.hibernate.validator.constraints.Email;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+/**
+ * Created by levuhuy on 12/22/16.
+ */
+public class EmailValidator implements ConstraintValidator<Email,String>{
+    @Override
+    public void initialize(Email paramA) {
+
+    }
+
+    @Override
+    public boolean isValid(String emailStr, ConstraintValidatorContext ctx) {
+        if (emailStr == null){
+            return false;
+        }
+
+        //validate phone numbers of format "1234567890"
+        if (emailStr.matches("^(0[1-9]|1[0-2])\\/(0[1-9]|1\\d|2\\d|3[01])\\/(19|20)\\d{2}$")) return true;
+
+        else return false;
+    }
+}
